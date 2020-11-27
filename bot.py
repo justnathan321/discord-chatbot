cbd50d0e874cb8fac5d373981d9edc33c5275d3c
Nathan Tetroashvili <nathan.tetroashvili@student.kdg.be>
2020-11-27 21:14:38 +0100

    added giphs, start msg changed

--- bot.py
+++ bot.py
@@ -7,9 +7,16 @@ from discord.ext import commands
 
 client = discord.Client()
 
-TOKEN = "NzgxOTQ2MDY4Njg2NDcxMTk4.X8FCBg.H-0m4UN0JwYfw0xIzwwsnlh67B0"
+TOKEN = ""
 
-greetings = ["Hey", "Whats up?", "I'm always here!", "Heya!", "I would like to!", "Let's have a chat"]
+msg_greetings = ["Hey", "Whats up?", "I'm always here!", "Heya! Let's talk.", "I would like to!", "Let's have a chat",
+                 "Hey, let's talk!",
+                 "https://giphy.com/gifs/raydonovan-season-6-episode-10-ray-donovan-57UyhoLuq6n81uiHkt",
+                 "https://giphy.com/gifs/raydonovan-season-7-ray-donovan-708-l3b1jcyA73gz3vveI8",
+                 "https://giphy.com/gifs/YourHappyWorkplace-your-happy-workplace-wendy-conrad-lets-talk-vfoz5YXYqyGcdoJCf1",
+                 "https://giphy.com/gifs/DrSquatchSoapCo-lets-talk-wecantalk-we-can-ehCBaednkq0KBpppwt"]
+
+msg_start_1 = "can someone talk"
 
 
 @client.event
@@ -18,8 +25,8 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("can someone talk with me"):
-        await message.channel.send(greetings[random.randint(0, 5)])
+    if message.content.startswith(msg_start_1):
+        await message.channel.send(msg_greetings[random.randint(0, len(msg_greetings) - 1)])
 
 
 @client.event
